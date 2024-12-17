33469985db6dd6f6edb231946f78e63ae8da1627
André Santos <andrefs@andrefs.com>
2024-12-17 22:25:24 +0000

    more tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -10,12 +10,34 @@ describe('spearman-rank-corr', function() {
     expect(value).toBeCloseTo(0.6829268292682927, 6)
   });
 
-  it('#1: -0.1758', function() {
+  it('#2: -0.1758', function() {
     let x = [106, 86, 100, 101, 99, 103, 97, 113, 112, 110];
     let y = [7, 0, 27, 50, 28, 29, 20, 12, 6, 17];
 
     const value = rho(x, y);
     expect(value).toBeCloseTo(-0.17575757575757575, 6)
   });
+
+  it('#3 -0.104867', function() {
+    let x = [5, 3, 1, 1, 1, 2, 4, 4, 3, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    let y = [4, 2, 1, 3, 1, 1, 1, 4, 3, 2, 1, 1, 3, 5, 5, 5, 2, 3, 3, 3];
+
+    const value = rho(x, y);
+    expect(value).toBeCloseTo(-0.1048674176511480, 6)
+  })
+  it('#3 0.370554', function() {
+    let y = [4, 2, 1, 3, 1, 1, 1, 4, 3, 2, 1, 1, 3, 5, 5, 5, 2, 3, 3, 3];
+    let z = [5, 3, 1, 1, 1, 1, 4, 3, 2, 1, 1, 1, 1, 1, 2, 4, 4, 3, 2, 2];
+
+    const value = rho(y, z);
+    expect(value).toBeCloseTo(0.37055425196656755, 6)
+  })
+  it('#3 0.365896', function() {
+    let x = [5, 3, 1, 1, 1, 2, 4, 4, 3, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    let z = [5, 3, 1, 1, 1, 1, 4, 3, 2, 1, 1, 1, 1, 1, 2, 4, 4, 3, 2, 2];
+
+    const value = rho(x, z);
+    expect(value).toBeCloseTo(0.3658959519697062, 6)
+  })
 
 });
